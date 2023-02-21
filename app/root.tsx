81951eb8a1dc2261a7a3bc40446208c03ea25a40
ratasorin <ratasorin0@gmail.com>
2023-02-22 00:17:18 +0200

    [Feature] Improve focus and scrollbar UI

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { ShouldRevalidateFunction } from "@remix-run/react";
 import {
   Links,
   LiveReload,
@@ -7,14 +8,16 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import tailwindStylesheetUrl from "./styles/tailwind.css";
+import tailwind from "./styles/tailwind.css";
+import scrollbar from "./styles/scrollbar.css";
+import focus from "./styles/focus.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "jotai";
 import { queryClient } from "./src/react-query";
 
 export const links: LinksFunction = () => {
   return [
-    { rel: "stylesheet", href: tailwindStylesheetUrl },
+    { rel: "stylesheet", href: tailwind },
     {
       rel: "stylesheet",
       href: "https://js.arcgis.com/4.25/@arcgis/core/assets/esri/themes/dark/main.css",
@@ -26,6 +29,14 @@ export const links: LinksFunction = () => {
     {
       rel: "stylesheet",
       href: "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css",
+    },
+    {
+      rel: "stylesheet",
+      href: scrollbar,
+    },
+    {
+      rel: "stylesheet",
+      href: focus,
     },
   ];
 };
